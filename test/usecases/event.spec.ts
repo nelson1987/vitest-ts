820b4bc1751666a7c2d2cbcea64eae58c83e5114
nelson.neto <nelson.neto@aarin.com.br>
2025-02-24 11:08:25 -0300

    tst: passing all case of mocks

--- test/usecases/event.spec.ts
+++ test/usecases/event.spec.ts
@@ -14,42 +14,46 @@ describe("Event Handler", () => {
     const result = eventHandler(testEvent);
     expect(result).toEqual("event");
   });
+
   it("should return the mocked event when the handler processes a mocked event", () => {
-    producer.mockReturnValueOnce("mockedEvent");
+    (producer as Mock).mockReturnValueOnce("mockedEvent");
     const result = eventHandler(testEvent);
     expect(result).toEqual("mockedEvent");
   });
+
   it("should return the mocked event when the handler processes a mocked event", () => {
-    producer.mockReturnValueOnce("mockedEvent_2");
+    (producer as Mock).mockReturnValueOnce("mockedEvent_2");
     const result = eventHandler(testEvent);
     expect(result).toEqual("mockedEvent_2");
   });
+
   it("should return the produced event when the handler processes an event", () => {
     const result = eventHandler(testEvent);
     expect(result).toEqual("event");
   });
 });
+
 describe("Producer", () => {
+  const testEvent = "event";
   it("when producer produce a event should return the event", () => {
-    const event = "event";
-    const result = producer(event);
+    const result = producer(testEvent);
     expect(result).toEqual("event");
   });
+
   it("when producer produce a mocked event should return the mocked event", () => {
-    const event = "event";
     (producer as Mock).mockReturnValueOnce("mockedEvent");
-    const result = producer(event);
+    const result = producer(testEvent);
     expect(result).toEqual("mockedEvent");
   });
+
   it("when producer produce a mocked event should return the mocked event", () => {
-    const event = "event";
-    producer.mockReturnValueOnce("mockedEvent_2");
-    const result = producer(event);
+    (producer as Mock).mockReturnValueOnce("mockedEvent_2");
+    const result = producer(testEvent);
     expect(result).toEqual("mockedEvent_2");
   });
+
   it("when producer produce a event should return the event", () => {
-    const event = "event";
-    const result = producer(event);
+    const result = producer(testEvent);
     expect(result).toEqual("event");
   });
 });
