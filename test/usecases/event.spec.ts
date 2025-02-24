b17b8e95a5cf6b90b5bd7f9e4e2dc8063ab85f4b
nelson.neto <nelson.neto@aarin.com.br>
2025-02-24 10:07:54 -0300

    feat: change in unit tests

--- test/usecases/event.spec.ts
+++ test/usecases/event.spec.ts
@@ -1,19 +1,37 @@
 import { describe, expect, it, vi } from "vitest";
 import eventHandler from "../../src/eventHandler";
+import producer from "../../src/producer";
 
-describe("event", () => {
-  it("when handler produce a mocked event should return the event", () => {
-    const event = "click";
-    const result = eventHandler(event);
-    expect(result).toEqual("erro");
-  });
-
+describe("Event Handler", () => {
   it("when handler produce a event should return the event", () => {
-    const event = "click";
+    const event = "event";
+    const result = eventHandler(event);
+    expect(result).toEqual("event");
+  });
+  it("when handler produce a mocked event should return the mocked event", () => {
+    const event = "event";
     vi.mock("../../src/producer", () => ({
-      default: vi.fn(() => "erro"),
+      default: vi.fn(() => "mockedEvent"),
     }));
     const result = eventHandler(event);
-    expect(result).toEqual(event);
+    expect(result).toEqual("mockedEvent");
+  });
+});
+
+describe("Producer", () => {
+  it("when producer produce a event should return the event", () => {
+    const event = "event";
+    const result = producer(event);
+    expect(result).toEqual("event");
+  });
+  it("when producer produce a mocked event should return the mocked event", () => {
+    const event = "event";
+    vi.fn(() => "mockedEvent");
+    // vi.mock("../../src/producer", () => ({
+    //   default: vi.fn(() => "mockedEvent"),
+    // }));
+    // vi.fn(producer).mockReturnValueOnce('mockedEvent');
+    const result = producer(event);
+    expect(result).toEqual("event");
   });
 });
