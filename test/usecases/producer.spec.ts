093461b73922c993f3c89f194e539a8b9002c79b
nelson.neto <nelson.neto@aarin.com.br>
2025-02-24 11:28:44 -0300

    tst: include mocks

--- test/usecases/producer.spec.ts
+++ test/usecases/producer.spec.ts
@@ -1,5 +1,4 @@
 import { describe, expect, it, Mock, vi } from "vitest";
-import eventHandler from "../../src/eventHandler";
 import producer from "../../src/producer";
 vi.mock("../../src/producer", async () => {
   const originalModule = await vi.importActual("../../src/producer");
@@ -7,28 +6,27 @@ vi.mock("../../src/producer", async () => {
     default: vi.fn(originalModule.default as never),
   };
 });
-
-describe("Event Handler", () => {
+describe("Producer", () => {
   const testEvent = "event";
-  it("should return the produced event when the handler processes an event", () => {
-    const result = eventHandler(testEvent);
+  it("when producer produce a event should return the event", () => {
+    const result = producer(testEvent);
     expect(result).toEqual("event");
   });
 
-  it("should return the mocked event when the handler processes a mocked event", () => {
+  it("when producer produce a mocked event should return the mocked event", () => {
     (producer as Mock).mockReturnValueOnce("mockedEvent");
-    const result = eventHandler(testEvent);
+    const result = producer(testEvent);
     expect(result).toEqual("mockedEvent");
   });
 
-  it("should return the mocked event when the handler processes a mocked event", () => {
+  it("when producer produce a mocked event should return the mocked event", () => {
     (producer as Mock).mockReturnValueOnce("mockedEvent_2");
-    const result = eventHandler(testEvent);
+    const result = producer(testEvent);
     expect(result).toEqual("mockedEvent_2");
   });
 
-  it("should return the produced event when the handler processes an event", () => {
-    const result = eventHandler(testEvent);
+  it("when producer produce a event should return the event", () => {
+    const result = producer(testEvent);
     expect(result).toEqual("event");
   });
 });
